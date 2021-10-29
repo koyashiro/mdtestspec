a9d4799540b9948d370e6406a7a0fdd02f77dbe4
koyashiro <develop@koyashi.ro>
2021-10-29 23:12:42 +0900

    feat: implement h1 - h4 parsing

--- pkg/md2xlsx/markdown.go
+++ pkg/md2xlsx/markdown.go
@@ -1,6 +1,7 @@
 package md2xlsx
 
 import (
+	"fmt"
 	"io/ioutil"
 
 	"github.com/gomarkdown/markdown/ast"
@@ -65,4 +66,98 @@ func getHeadingContent(heading ast.Node) string {
 		}
 	}
 	return ""
+}
+
+func parseHeading1(heading *ast.Heading) *Spec {
+	s := &Spec{}
+
+	if heading.Level != 1 {
+		panic(fmt.Sprintf("Heading level is not 1, %d", heading.Level))
+	}
+
+	for _, n := range heading.Container.GetChildren() {
+		if t, ok := n.(*ast.Text); ok {
+			if l := string(t.Literal); l != "" {
+				s.Name = l
+			}
+			continue
+		}
+
+		if h, ok := n.(*ast.Heading); ok && h.Level == 2 {
+			c := parseHeading2(h)
+			s.Categories = append(s.Categories, c)
+			continue
+		}
+	}
+
+	return s
+}
+
+func parseHeading2(heading *ast.Heading) *Category {
+	c := &Category{}
+
+	if heading.Level != 2 {
+		panic(fmt.Sprintf("Heading level is not 2, heading level: %d", heading.Level))
+	}
+
+	for _, n := range heading.Container.GetChildren() {
+		if t, ok := n.(*ast.Text); ok {
+			if l := string(t.Literal); l != "" {
+				c.Name = l
+			}
+			continue
+		}
+
+		if h, ok := n.(*ast.Heading); ok && h.Level == 3 {
+			sc := parseHeading3(h)
+			c.SubCategories = append(c.SubCategories, sc)
+			continue
+		}
+	}
+
+	return c
+}
+
+func parseHeading3(heading *ast.Heading) *SubCategory {
+	sc := &SubCategory{}
+
+	if heading.Level != 3 {
+		panic(fmt.Sprintf("Heading level is not 3, heading level: %d", heading.Level))
+	}
+
+	for _, n := range heading.Container.GetChildren() {
+		if t, ok := n.(*ast.Text); ok {
+			if l := string(t.Literal); l != "" {
+				sc.Name = l
+			}
+			continue
+		}
+
+		if h, ok := n.(*ast.Heading); ok && h.Level == 3 {
+			ssc := parseHeading4(h)
+			sc.SubSubCategories = append(sc.SubSubCategories, ssc)
+			continue
+		}
+	}
+
+	return sc
+}
+
+func parseHeading4(heading *ast.Heading) *SubSubCategory {
+	ssc := &SubSubCategory{}
+
+	if heading.Level != 4 {
+		panic(fmt.Sprintf("Heading level is not 4, heading level: %d", heading.Level))
+	}
+
+	for _, n := range heading.Container.GetChildren() {
+		if t, ok := n.(*ast.Text); ok {
+			if l := string(t.Literal); l != "" {
+				ssc.Name = l
+			}
+			continue
+		}
+	}
+
+	return ssc
 }
--- pkg/md2xlsx/markdown_test.go
+++ pkg/md2xlsx/markdown_test.go
@@ -1,18 +1,70 @@
-package md2xlsx_test
+package md2xlsx
 
 import (
 	"testing"
 
-	"github.com/koyashiro/md2xlsx/pkg/md2xlsx"
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/parser"
 )
 
-var p = md2xlsx.NewMarkdownParser()
+func TestParseHeading1(t *testing.T) {
+	input := `
+# Spec
+`
+	p := parser.New()
+	n := p.Parse([]byte(input)).GetChildren()[0]
+	if h, ok := n.(*ast.Heading); ok {
+		s := parseHeading1(h)
+		if s.Name != "Spec" {
+			t.Errorf("s.Name = %v, want %v", s.Name, "Spec")
+		}
+	} else {
+		t.Errorf("ok = %v, want %v", ok, true)
+	}
+}
 
-func TestParseHeading(t *testing.T) {
-	input := "# Heading"
-	md := p.Parse([]byte(input))
-	s := md.AsSpec()
-	if s.Name != "Heading" {
-		t.Errorf("s.Name = %v, want %v", s.Name, "Heading")
+func TestParseHeading2(t *testing.T) {
+	input := `
+## Category
+`
+	p := parser.New()
+	n := p.Parse([]byte(input)).GetChildren()[0]
+	if h, ok := n.(*ast.Heading); ok {
+		c := parseHeading2(h)
+		if c.Name != "Category" {
+			t.Errorf("c.Name = %v, want %v", c.Name, "Category")
+		}
+	} else {
+		t.Errorf("ok = %v, want %v", ok, true)
+	}
+}
+
+func TestParseHeading3(t *testing.T) {
+	input := "### SubCategory"
+	p := parser.New()
+	n := p.Parse([]byte(input)).GetChildren()[0]
+	if h, ok := n.(*ast.Heading); ok {
+		sc := parseHeading3(h)
+		if sc.Name != "SubCategory" {
+			t.Errorf("sc.Name = %v, want %v", sc.Name, "SubCategory")
+		}
+	} else {
+		t.Errorf("ok = %v, want %v", ok, true)
+	}
+}
+
+func TestParseHeading4(t *testing.T) {
+	input := `
+#### SubSubCategory
+`
+	p := parser.New()
+	n := p.Parse([]byte(input)).GetChildren()[0]
+	if h, ok := n.(*ast.Heading); ok {
+		ssc := parseHeading4(h)
+		if ssc.Name != "SubSubCategory" {
+			t.Errorf("ssc.Name = %v, want %v", ssc.Name, "SubSubCategory")
+		}
+	} else {
+		t.Errorf("ok = %v, want %v", ok, true)
 	}
 }
