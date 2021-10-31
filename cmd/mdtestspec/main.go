c7b314644010e458edf79ebcefd156164d419935
koyashiro <develop@koyashi.ro>
2021-10-31 20:33:30 +0900

    refactor: update directory structure

---
+++ cmd/mdtestspec/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/koyashiro/mdtestspec/internal/cli"
+
+func main() {
+	cli.Execute()
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/koyashiro/mdtestspec/cmd"
-
-func main() {
-	cmd.Execute()
-}
